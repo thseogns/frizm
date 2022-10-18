5573714eac2c3d84a0fc6854e7016eb6c0dd6b82
thseogns <99688960+thseogns@users.noreply.github.com>
2022-10-19 02:32:09 +0900

    10-19일

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 const btnArrow = document.querySelectorAll(".btn button");
-const btn = document.querySelector(".btn");
+
+const s_btnArrow = document.querySelectorAll("#s_btn");
 
 for (let i = 0; i < btnArrow.length; i++) {
   btnArrow[i].addEventListener("click", function () {
+    if (i == 0) {
+      console.log(0);
+    } else {
+      console.log(1);
+    }
+  });
+}
+for (let i = 0; i < s_btnArrow.length; i++) {
+  s_btnArrow[i].addEventListener("click", function () {
     if (i == 0) {
       console.log(0);
     } else {
